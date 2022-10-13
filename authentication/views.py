bb9962d3d81acdce9412cafe4e3499771324056d
slv-improver <k.moret.slv@gmail.com>
2022-10-14 00:51:42 +0100

    add signup page

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def signup(r):
+    return render(
+        r,
+        'authentication/signup.html',
+    )
--- litreview/urls.py
+++ litreview/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from authentication import views as auth_views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("signup/", auth_views.signup, name='signup'),
 ]
