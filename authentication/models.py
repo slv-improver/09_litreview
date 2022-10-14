e97f1b040f4e3819783f53996039ae87023f9c34
slv-improver <k.moret.slv@gmail.com>
2022-10-14 12:13:30 +0100

    create User & UserFollows models

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class UserFollows(models.Model):
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+    followed_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='followed_by'
+    )
+
+    class Meta:
+        unique_together = ('user', 'followed_user')
