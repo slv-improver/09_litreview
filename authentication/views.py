df2e1f1b2bdce67b5a408690446aada33315d793
slv-improver <k.moret.slv@gmail.com>
2022-10-15 19:42:16 +0100

    add SignupForm to register new user

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from . import forms
 
 
 def signup(r):
+    if r.method == 'POST':
+        form = forms.SignupForm(r.POST)
+        if form.is_valid():
+            user = form.save()
+    else:
+        form = forms.SignupForm()
     return render(
         r,
         'authentication/signup.html',
+        {'form': form}
     )
