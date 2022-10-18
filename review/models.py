f82db7e469d4ccb26739afa055e3699e1dbe8f82
slv-improver <k.moret.slv@gmail.com>
2022-10-18 14:51:59 +0100

    resize image before storing it

--- review/models.py
+++ review/models.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+from PIL import Image
+
 
 class Ticket(models.Model):
     title = models.CharField(max_length=128)
@@ -12,6 +14,17 @@ class Ticket(models.Model):
     )
     image = models.ImageField(null=True, blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
+
+    IMAGE_MAX_SIZE = (500,500)
+
+    def resize_image(self):
+        image = Image.open(self.image)
+        image.thumbnail(self.IMAGE_MAX_SIZE)
+        image.save(self.image.path)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.resize_image()
 
 
 class Review(models.Model):
