e74c715ad2555d8134932fdfb0bc9d621aa6ed3d
slv-improver <k.moret.slv@gmail.com>
2022-10-20 23:59:05 +0100

    add RadioSelect widget for rating

--- review/forms.py
+++ review/forms.py
@@ -10,4 +10,9 @@ class TicketForm(forms.ModelForm):
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = models.Review
-        fields = ('headline', 'body', 'rating')
+        fields = ('headline', 'rating', 'body')
+        widgets = {
+            'rating': forms.RadioSelect(choices=[
+                (0, '0'), (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')
+            ], attrs={ 'class': 'star-rating'}),
+        }
