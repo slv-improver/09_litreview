87ce2fe06baa9906202bd1e2dcf3302eb2fb40d9
slv-improver <k.moret.slv@gmail.com>
2022-10-25 22:48:36 +0100

    format date with extras filter

--- review/templatetags/review_extras.py
+++ review/templatetags/review_extras.py
@@ -12,3 +12,7 @@ def get_poster_display(context, user):
     if user == context['user']:
         return 'You'
     return user.username
+
+@register.filter
+def format_date(time):
+    return time.strftime("%I:%M%p, %-d %B %y")
