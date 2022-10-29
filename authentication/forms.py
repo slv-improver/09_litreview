81c5577f21fba474519fe827fc22442f9918a582
slv-improver <k.moret.slv@gmail.com>
2022-10-29 10:46:21 +0100

    make label as placeholder in login & signup form

--- authentication/forms.py
+++ authentication/forms.py
@@ -4,10 +4,27 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class SignupForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].label = ''
+            self.fields[field].help_text = None
+            self.fields[field].widget.attrs.update({
+                'placeholder': field.capitalize(),
+            })
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('username',)
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=63, label='Username')
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Password')
+    username = forms.CharField(max_length=63, label='')
+    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='')
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'placeholder': field.capitalize(),
+            })
